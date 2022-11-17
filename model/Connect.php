5af124bd2ac905e4be7bdc23e74bf90c1664c571
mohamed67210 <mohamed_bechri@outlook.fr>
2022-11-17 13:56:11 +0100

    new file:   controller/CinemaController.php new file:   index.php 	new file:   model/Connect.php 	new file:   view/template.php

---
+++ model/Connect.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Model;
+
+abstract class Connect{
+    const HOST = "localhost";
+    const DB = "cinema";
+    const USER = "root";
+    const MDP = "";
+
+    public static function seConnecter(){
+        try{
+            return new \PDO ("mysql:host=".self::HOST.";dbname=".self::DB.";charset=utf8", self::USER, self::MDP);
+        }catch(\PDOException $ex){
+            return $ex->getMessage();
+        }
+    }
+}
